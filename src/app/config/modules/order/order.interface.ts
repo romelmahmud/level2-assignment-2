96681e5783e240630b908b406c95e66a52029a71
romel <romelmahmud.dev@gmail.com>
2024-05-22 18:24:35 +0600

    order interface model create

--- src/app/config/modules/order/order.interface.ts
+++ src/app/config/modules/order/order.interface.ts
@@ -1,1 +1,6 @@
-// order interface
+export interface TOrder {
+  email: string;
+  productId: string;
+  price: number;
+  quantity: number;
+}
--- src/app/config/modules/order/order.model.ts
+++ src/app/config/modules/order/order.model.ts
@@ -1,1 +1,23 @@
-// order model
+import { Schema, model } from 'mongoose';
+import { TOrder } from './order.interface';
+
+const orderSchema = new Schema<TOrder>({
+  email: {
+    type: String,
+    required: [true, 'email is required'],
+  },
+  productId: {
+    type: String,
+    required: [true, 'productId is required'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'product price is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'product quantity is required'],
+  },
+});
+
+export const Order = model('Order', orderSchema);
