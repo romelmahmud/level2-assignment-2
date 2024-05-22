205939892fcadb9a51bfdfe4de79c7bff7a7f088
romel <romelmahmud.dev@gmail.com>
2024-05-22 15:51:52 +0600

    product interface created

--- src/app/config/modules/product/product.interface.ts
+++ src/app/config/modules/product/product.interface.ts
@@ -1,1 +1,20 @@
 // product interface
+
+interface TVariant {
+  type: string;
+  value: string;
+}
+
+interface TInventory {
+  quantity: number;
+  inStock: boolean;
+}
+export interface TProduct {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: [string];
+  variant: TVariant;
+  inventory: TInventory;
+}
