e997cf09200642096f8a4162fc58902ee99bb909
romel <romelmahmud.dev@gmail.com>
2024-05-22 16:30:54 +0600

    basic controller funtion created

--- src/app/config/modules/product/product.controller.ts
+++ src/app/config/modules/product/product.controller.ts
@@ -1,1 +1,35 @@
 // product controller
+
+import { Request, Response } from 'express';
+
+const createProduct = async (req: Request, res: Response) => {
+  console.log('product created');
+};
+
+const getAllProducts = async (req: Request, res: Response) => {
+  if (req.params) {
+    console.log('got a specific product');
+  } else {
+    console.log('all product got');
+  }
+};
+
+const updateProduct = async (req: Request, res: Response) => {
+  console.log('product updated');
+};
+
+const deleteProduct = async (req: Request, res: Response) => {
+  console.log('product deleted');
+};
+
+const searchProduct = async (req: Request, res: Response) => {
+  console.log('product searching');
+};
+
+export const ProductControllers = {
+  createProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+  searchProduct,
+};
